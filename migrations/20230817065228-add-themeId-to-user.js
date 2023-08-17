3b5da2c4707ea521bc21058e17fe7de4f9f4827b
klu0926 <lukuoyu@gmail.com>
2023-08-17 15:17:20 +0800

    feat: add themeId to users

---
+++ migrations/20230817065228-add-themeId-to-user.js
@@ -0,0 +1,19 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    await queryInterface.addColumn('Users', 'themeId', {
+      type: Sequelize.INTEGER,
+      allowNull: false,
+      references: {
+        model: 'Users',
+        key: 'id'
+      }
+    })
+  },
+
+  async down (queryInterface, Sequelize) {
+    await queryInterface.removeColumn('Users', 'themeId')
+  }
+};
