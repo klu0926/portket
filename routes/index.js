d7bfe7f8c97c4f2dcb9e0ae018af16713cc2bc1e
klu0926 <lukuoyu@gmail.com>
2023-08-17 15:32:57 +0800

    feat: create user router and install some packages

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,14 @@
 const router = require('express').Router()
+const user = require('./modules/user')
 
 
+
+// router.post('/login' )
+// router.post('/signup' )
+// router.post('/logout')
+
+
+router.use('/users', user)
 router.get('', (req, res)=> {
   res.send('Hello world!')
 })
