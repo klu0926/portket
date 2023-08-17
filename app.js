35fa7e03ce577dc7e8729e339298f8c6ce083f53
klu0926 <lukuoyu@gmail.com>
2023-08-17 15:43:37 +0800

    feat: setup app

--- app.js
+++ app.js
@@ -1,13 +1,21 @@
 const express = require("express")
-const routes = require("./routes")
-
 const app = express()
 const port = process.env.PORT || 3000
+const methodOverride = require('method-override')
+const routes = require("./routes")
 
+// view engine
+const handlebars = require("express-handlebars")
+app.engine('hbs', handlebars({defaultLayout: 'main', extends: '.hbs'}))
+app.set('view engine', 'hbs')
+
+// use
+app.use(express.urlencoded({extended: true}))
+app.use(methodOverride('_method'))
+
+// routes
 app.use(routes)
 
 app.listen(port, () => {
   console.info(`Server running on port ${port}`)
 })
-
-module.exports = app
