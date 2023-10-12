57bfd5b8fdf57c3370310f3fcb780b07775bdbe7
klu0926 <lukuoyu@gmail.com>
2023-10-12 16:19:01 +0800

    feat: add edit mode, and block add project btn when in edit mode

--- public/javascript/myPortfolio.js
+++ public/javascript/myPortfolio.js
@@ -1,46 +1,84 @@
 document.addEventListener('DOMContentLoaded', () => {
-  editUserAvatar()
+  previewAvatar()
   editMode()
 })
 
-function editUserAvatar() {
-  const avatarContainer = document.querySelector('#avatar-container')
-  const avatarForm = document.querySelector('#avatar-form')
+function previewAvatar() {
+  const avatarInputDiv = document.querySelector('#avatar-input-div')
   const avatarInput = document.querySelector('#avatar-input')
-  avatarContainer.addEventListener('click', (event) => {
+  const avatarImg = document.querySelector('#avatar-img')
+  avatarInputDiv.addEventListener('click', (event) => {
     avatarInput.click()
   })
-
   avatarInput.addEventListener('change', () => {
-    avatarForm.submit()
+    if (avatarInput.files && avatarInput.files[0]) {
+      const reader = new FileReader()
+      reader.onload = (event) => {
+        avatarImg.src = event.target.result
+      }
+      reader.readAsDataURL(avatarInput.files[0])
+    }
   })
 }
 
 function editMode() {
+  // edit nav bar
   const editPortfolioBtn = document.querySelector('#edit-portfolio-btn')
   const savePortfolioBtn = document.querySelector('#save-portfolio-btn')
   const cancelPortfolioBtn = document.querySelector('#cancel-portfolio-btn')
   const editModeDisplay = document.querySelector('#edit-mode-display')
+  // input div
+  const avatarInputDiv = document.querySelector('#avatar-input-div')
+  const nameInputDiv = document.querySelector('#name-input-div')
+  const titleInputDiv = document.querySelector('#title-input-div')
+  const socialInputDiv = document.querySelector('#social-input-div')
+  // display
+  const nameDisplay = document.querySelector('#name-display')
+  const titleDisplay = document.querySelector('#title-display')
+  const socialDisplay = document.querySelector('#social-display')
+  // project btn
+  const addProjectBtn = document.querySelector('#add-project-btn')
+  const projectBlocker = document.querySelector('#project-blocker')
+
+  // items list
+  const editModeItems = [nameInputDiv, titleInputDiv, socialInputDiv, avatarInputDiv, projectBlocker]
+  const viewModeItems = [nameDisplay, titleDisplay, socialDisplay, addProjectBtn]
 
   editPortfolioBtn.addEventListener('click', () => {
     // show
     savePortfolioBtn.style.display = 'flex'
     cancelPortfolioBtn.style.display = 'flex'
+    editModeItems.forEach((e) => {
+      e.style.display = 'block'
+    })
     // hide
     editPortfolioBtn.style.display = 'none'
-    // play edit mode animation
+    viewModeItems.forEach((e) => (e.style.display = 'none'))
+    viewModeItems.forEach((e) => {
+      e.style.display = 'none'
+    })
+    // show edit mode animation
     editModeDisplay.style.display = 'flex'
+    editModeDisplay.classList.add('show-animation')
+    editModeDisplay.classList.remove('hide-animation')
     editModeDisplay.style.animationPlayState = 'running'
   })
 
   cancelPortfolioBtn.addEventListener('click', () => {
     // show
     editPortfolioBtn.style.display = 'flex'
+    viewModeItems.forEach((e) => {
+      e.style.display = 'block'
+    })
     // hide
     savePortfolioBtn.style.display = 'none'
     cancelPortfolioBtn.style.display = 'none'
-    // reset edit mode animation
-    editModeDisplay.style.display = 'none'
-    editModeDisplay.style.animationPlayState = 'paused'
+    editModeItems.forEach((e) => {
+      e.style.display = 'none'
+    })
+    // hide edit mode animation
+    editModeDisplay.classList.remove('show-animation')
+    editModeDisplay.classList.add('hide-animation')
+    editModeDisplay.style.animationPlayState = 'running'
   })
 }
