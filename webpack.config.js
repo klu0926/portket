402b79158ff726562fb008d2a2c5a1bb2b536e02
klu0926 <lukuoyu@gmail.com>
2023-12-28 16:20:19 +0800

    feat: use webpack to bundle all my js file

---
+++ webpack.config.js
@@ -0,0 +1,15 @@
+const path = require('path')
+const { glob } = require('glob')
+
+module.exports = {
+  mode: 'production',
+  entry: glob.sync('./public/javascript/*.js').reduce((entries, entry) => {
+    const entryName = path.basename(entry, path.extname(entry))
+    entries[entryName] = `./${entry}`
+    return entries
+  }, {}),
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'public/dist'),
+  },
+}
